fb4e8f94e3c53171e00bee1b006b95a93be97d95
Matt Abraham <mabe3069@mail.com>
2023-08-07 20:14:19 -0400

    Adding working version of game back to GitHub Master branch

---
+++ level_1.py
@@ -0,0 +1,139 @@
+import pygame
+import random
+import sys
+import os
+import csv
+
+pygame.init()
+
+# # Screen dimensions
+# WIDTH, HEIGHT = 800, 600
+FPS = 60
+
+# Constants for the screen dimensions
+WIDTH = 1450
+HEIGHT = 850
+
+# Colors
+WHITE = (255, 255, 255)
+TRANSPARENT = (0, 0, 0, 0)
+
+SQUARE_SIZE = 30
+
+# List of letters
+LETTERS = [chr(ord('a') + i) for i in range(26)]
+
+# Load images of letters
+letter_images = {}
+assets_path = r"C:\Users\HongM\fantastic-four-first\Typing_Game\Assets" #change to whatever your filepath is
+for letter in LETTERS:
+    image_path = os.path.join(assets_path, f"{letter.upper()}_KEY.png")
+    try:
+        letter_images[letter.upper()] = pygame.image.load(image_path)
+    except pygame.error:
+        print(
+            f"Image file {image_path} not found for letter {letter}. Make sure to have all letter images from A to Z.")
+        sys.exit(1)
+
+
+class Shape:
+    def __init__(self, letter, spawn_time):
+        self.shape = pygame.Rect(0, 0, SQUARE_SIZE, SQUARE_SIZE)
+        self.shape.x = random.randint(0, WIDTH - self.shape.width)
+        self.shape.y = -self.shape.height
+        self.letter = letter.upper()
+        self.spawn_time = spawn_time
+        self.falling_speed = 5  # Adjust the falling speed here (change this value as needed)
+
+    def move(self):
+        self.shape.y += self.falling_speed
+
+    def draw(self, surface):
+        letter_image = letter_images.get(self.letter)
+        if letter_image:
+            letter_image = pygame.transform.scale(letter_image, (SQUARE_SIZE, SQUARE_SIZE))
+            surface.blit(letter_image, self.shape.topleft)
+        else:
+            pygame.draw.rect(surface, WHITE, self.shape)
+
+    def is_in_hitbox(self):
+        hitbox_bottom = HEIGHT // 8
+        return self.shape.y + self.shape.height >= HEIGHT - hitbox_bottom
+
+
+def level_1():
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Prototype")
+    clock = pygame.time.Clock()
+
+    # Load background image
+    background_image = pygame.image.load(os.path.join("Assets", "gg.jpg"))
+    background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
+
+    shapes = []
+    SPAWN_INTERVAL = 2000  # 2000ms (2 seconds) interval
+    last_spawn_time = 0
+
+    letter_index = 0
+
+    score = 0
+    score_font = pygame.font.Font(None, 36)
+
+    # Initialize Pygame mixer for background music
+    pygame.mixer.init()
+
+    # Load and play the background music
+    background_music_file = os.path.join("Music", "Song_1.mp3")
+    pygame.mixer.music.load(background_music_file)
+    pygame.mixer.music.play(-1)  # -1 means loop the music
+
+    while True:
+        pressed_key = None
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.unicode:  # checks if the key corresponds to a character
+                    pressed_key = event.unicode.upper()  # normalize to uppercase
+
+        if pressed_key:
+            for shape in shapes[:]:  # make a copy of the list to avoid issues while removing items
+                if shape.letter == pressed_key and shape.is_in_hitbox():
+                    shapes.remove(shape)
+                    score += 1
+
+        current_time = pygame.time.get_ticks()
+
+        # Check if it's time to spawn a new shape
+        if current_time - last_spawn_time >= SPAWN_INTERVAL:
+            letter = random.choice(LETTERS)
+            shapes.append(Shape(letter, current_time))
+            last_spawn_time = current_time
+            letter_index = (letter_index + 1) % len(LETTERS)
+
+        # Draw background image
+        screen.blit(background_image, (0, 0))
+
+        # rectangle on the bottom of the screen
+        bottom_rect_height = HEIGHT // 8
+        bottom_rect = pygame.Surface((WIDTH, bottom_rect_height), pygame.SRCALPHA)
+        pygame.draw.rect(bottom_rect, TRANSPARENT, (0, 0, WIDTH, bottom_rect_height))
+        pygame.draw.rect(bottom_rect, WHITE, (2, 2, WIDTH - 4, bottom_rect_height - 4), 2)
+        screen.blit(bottom_rect, (0, HEIGHT - bottom_rect_height))
+
+        # Move and draw the shapes
+        for shape_obj in shapes:
+            shape_obj.move()
+            shape_obj.draw(screen)
+
+        # Draw score
+        score_text = score_font.render(f"Score: {score}", True, WHITE)
+        screen.blit(score_text, (10, 10))
+
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+#if __name__ == "__main__":
+    #main()
---
+++ main.py
@@ -0,0 +1,606 @@
+import pygame
+import random
+import math
+import level_1
+
+
+pygame.init()
+
+WHITE = (255, 255, 255)
+PURPLE = (51, 5, 99)
+TITLE = (217, 177, 33)
+YELLOW = (201, 196, 26)
+
+# Constants for the screen dimensions
+SCREEN_WIDTH = 1450
+SCREEN_HEIGHT = 850
+BUTTON_RADIUS = 50
+
+
+
+
+# Constants for the button dimensions
+BUTTON_WIDTH = 10
+BUTTON_HEIGHT = 10
+
+
+
+class CircleButton:
+    def __init__(self, x, y, radius, color, text, text_color, font_size):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.text = text
+        self.text_color = text_color
+        self.font = pygame.font.Font(None, font_size)
+
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
+        text_surface = self.font.render(self.text, True, self.text_color)
+        text_rect = text_surface.get_rect(center=(self.x, self.y))
+        screen.blit(text_surface, text_rect)
+
+    def is_clicked(self, pos):
+        distance = math.sqrt((self.x - pos[0]) ** 2 + (self.y - pos[1]) ** 2)
+        return distance <= self.radius
+
+# Create the Pygame window
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Circle Button Example")
+
+# Create the button
+button2 = CircleButton(
+    x=SCREEN_WIDTH // 2,
+    y=SCREEN_HEIGHT // 2,
+    radius=BUTTON_RADIUS,
+    color=PURPLE,
+    text="Level-1",
+    text_color=WHITE,
+    font_size=35
+)
+
+button1 = CircleButton(
+    x=SCREEN_WIDTH // 2,
+    y=(SCREEN_HEIGHT - SCREEN_HEIGHT//2)+110,
+    radius=BUTTON_RADIUS,
+    color=PURPLE,
+    text="Level-2",
+    text_color=WHITE,
+    font_size=30
+)
+
+button3 = CircleButton(
+    x=SCREEN_WIDTH // 2,
+    y=(SCREEN_HEIGHT - SCREEN_HEIGHT//2)+220,
+    radius=BUTTON_RADIUS,
+    color=PURPLE,
+    text="Exit",
+    text_color=WHITE,
+    font_size=30
+)
+
+
+
+# Image imports
+BG = pygame.image.load("D.png")
+target = pygame.image.load("shot.png")
+#shotty = pygame.image.load("Shotgun.png")
+shot = pygame.image.load("Shot.png")
+#cowboy = pygame.image.load("Graphics\Cowboy.png")
+
+# Title and icon
+pygame.display.set_caption("Lucky onions!")
+icon = pygame.image.load("onion.png")
+pygame.display.set_icon(icon)
+
+# Clock
+clock = pygame.time.Clock()
+
+
+# Text font
+font = pygame.font.Font("freesansbold.ttf", 35)
+
+# Target coordinates, these two values should change when the new word appears
+textX = 190
+textY = 450
+
+# Current word that we are typing. It has an indexed version as well so we can update as we type
+currentWord = " "
+OGword = " "
+indexedWord = list(currentWord)
+currentLength = len(indexedWord)
+lengthTracker = 0
+starttrack = 0  # used to have the intro screen only appear at the begininning
+
+# Random words (This can be changed to any list of words)
+words = [
+    "asdf",
+    "throne",
+    "thrill",
+    "long",
+    "size",
+    "hair",
+    "plan",
+    "nauseating",
+    "pull",
+    "cagey",
+    "berserk",
+    "balance",
+    "previous",
+    "ship",
+    "phone",
+    "icky",
+    "balance",
+    "private",
+    "hysterical",
+    "ashamed",
+    "bike",
+    "sun",
+    "better",
+    "early",
+    "glow",
+    "chase",
+    "verse",
+    "stale",
+    "point",
+    "milky",
+    "futuristic",
+    "cushion",
+    "clam",
+    "wait",
+    "flesh",
+    "peaceful",
+    "limit",
+    "bat",
+    "wound",
+    "bag",
+    "odd",
+    "cave",
+    "authority",
+    "wink",
+    "apparel",
+    "settle",
+    "debonair",
+    "turn",
+    "slip",
+    "seal",
+]
+
+# Scoreboard
+score = 0
+scoreboard = "Words typed: " + str(score)
+
+# All of the functions used in the program
+
+
+def printer(x, y):
+    """ Presents the text onto the screen."""
+    show = font.render(currentWord, True, (YELLOW))
+    screen.blit(show, (x, y))
+
+
+def updatescore(scoreboard):
+    """ Updates the score"""
+    show = font.render((scoreboard), True, (WHITE))  # pygame surface string
+    screen.blit(show, (1100, 40))
+
+
+def intro():
+    """ Lays out everything for the starting screen"""
+    CX = 50
+    show1 = font.render(("Welcome to prototype,"), True, TITLE )  # pygame surface string
+    show2 = font.render(("Are you ready to learn how to type?"), True, TITLE)  # pygame surface string
+    show3 = font.render(("Let have fun and learn more!!!"), True, TITLE)  # pygame surface string
+
+    screen.blit(show1, (70, 70))
+    screen.blit(show2, (70, 120))
+    screen.blit(show3, (70, 170))
+
+
+    button1.draw(screen)
+    button2.draw(screen)
+    button3.draw(screen)
+
+
+def randomString():
+    """Generate a random string of fixed length """
+    num = random.randrange(50)
+    return words[num]
+
+
+def randomCoordinates():
+    """Generates random coordinates to use when spawning a new word in"""
+    textX = random.randrange(550)
+    textY = random.randrange(125, 350)
+    return textX, textY
+
+
+def correctLetter():
+    """Eliminates the first letter of the word when the user is correct"""
+    global textX
+    global lengthTracker
+    global indexedWord
+    global currentWord
+    del indexedWord[0]
+    temp = ""  # stores the joined string
+    currentWord = temp.join(indexedWord)
+    lengthTracker += 1  # used to keep track of which letter the user is on
+
+
+def wrongLetter():
+    """When a letter is wrong, we want to restart and print out the original word."""
+    global OGword
+    global currentWord
+    global OGindex
+    global indexedWord
+    global lengthTracker
+    # reseting the word length
+    currentWord = OGword
+    indexedWord = OGindex.copy()
+    lengthTracker = 0  # resets the letter tracker
+
+
+# Loops through the game
+tracker = True
+while tracker:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            tracker = False  # This causes the While loop to evaluate False and exit the game
+
+
+
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            # Check if the left mouse button was clicked
+            mouse_pos = pygame.mouse.get_pos()
+            if button1.is_clicked(mouse_pos):
+                print("Circle Button Clicked!")
+                starttrack += 1
+                correctLetter()
+
+            if button2.is_clicked(mouse_pos):
+                print("Circle Button Clicked!")
+                #starttrack += 1
+                level_1.level_1()
+
+
+            if button3.is_clicked(mouse_pos):
+                print("Circle Button Clicked!")
+                tracker = False
+
+        # This is tracking if a key is pressed
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "a":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            # Each elif statement is for a certain letter on the keyboard
+            elif event.key == pygame.K_b:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "b":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_c:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "c":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_d:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "d":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_e:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "e":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_f:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "f":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_g:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "g":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_h:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "h":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_i:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "i":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_j:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "j":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_k:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "k":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_l:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "l":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_m:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "m":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_n:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "n":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_o:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "o":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_p:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "p":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_q:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "q":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_r:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "r":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_s:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "s":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_t:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "t":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_u:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "u":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_v:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "v":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_w:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "w":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_x:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "x":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_y:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "y":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_z:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == "z":
+                    correctLetter()
+                else:
+                    wrongLetter()
+
+            elif event.key == pygame.K_SPACE:
+                # if this key is pressed, we need to check if the  letter is the zero index
+                if indexedWord[0] == " ":
+                    correctLetter()
+
+    # basic updates
+    screen.fill((27, 150, 44))
+    screen.blit(BG, (0, 0))
+    printer(textX, textY)
+    updatescore(scoreboard)
+
+
+    if starttrack == 0:
+        # This loop only occurs at the start of the game
+        intro()
+    if lengthTracker == currentLength:
+        # this activates if the user types a whole word correctly
+        IMAGE_TIME = 30
+        if IMAGE_TIME > 0 and (starttrack != 0):
+            screen.blit(shot, (textX - 10, textY - 100))
+            IMAGE_TIME -= 1
+            score += 1
+
+        starttrack += 1  # used to exit loading screen in the begining
+
+        # sets up for the new word by slecting a new one and updating the score
+        scoreboard = "Words typed: " + str(score)  # updates the scoreboard
+        currentWord = randomString()  # choses a new word randomly
+        OGword = currentWord  # this keeps the original random string
+        indexedWord = list(currentWord)
+        OGindex = indexedWord.copy()
+        currentLength = len(indexedWord)
+        lengthTracker = 0  # resets the tracker for the next word
+        textX, textY = randomCoordinates()  # new target location
+
+    if starttrack != 0:
+        # this spawns shotgun and a new target
+        screen.blit(target, (textX - 10, textY - 100))
+        #screen.blit(shotty, (250, 350))
+    clock.tick(60)  # this is to control the length of the bullet muzzle flash
+    pygame.display.update()
+
+
+
+# Things to do in future possibly:
+# word changes with cool sound effect, like a bullet firing
+# word moves around screen
+# randomly generates a word with a generator instead of a predisposed list
+# add a starting menu with advanced options
+# calculate words per minute
+# Speed Letter Recognition game mode
+# paragraph calculator that calculates words per minute
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
