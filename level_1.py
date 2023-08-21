a78e24a91d8535bed274578ebaae9701f6b41356
hmhuynh1 <71050510+hmhuynh1@users.noreply.github.com>
2023-08-20 20:24:15 -0500

    Add update final project

--- level_1.py
+++ level_1.py
@@ -2,9 +2,15 @@ import pygame
 import random
 import sys
 import os
-
+import csv
 pygame.init()
 
+from pygame.locals import(
+    K_BACKSPACE,
+    K_ESCAPE,
+    K_DELETE,
+)
+#this initializes the backspace (restarting) escape (pausing) and f1 for force quiting respectivly
 FPS = 60
 WIDTH = 1400
 HEIGHT = 850
@@ -19,7 +25,8 @@ YELLOW = (255, 255, 0)
 
 LETTERS = [chr(ord('a') + i) for i in range(26)]
 letter_images = {}
-assets_path = r"C:\Users\HongM\Hong\TheLuckyOnions\Assets"
+
+assets_path = r"Assets"
 
 for letter in LETTERS:
     image_path = os.path.join(assets_path, f"{letter.upper()}_KEY.png")
@@ -29,7 +36,19 @@ for letter in LETTERS:
         print(
             f"Image file {image_path} not found for letter {letter}. Make sure to have all letter images from A to Z.")
         sys.exit(1)
+class Pause:
+    def __init__(self):
+        self.pause = False
 
+    def pause_game(self):
+        self.pause = True
+
+    def unpause_game(self):
+        self.pause = False
+
+    def pause_state(self):
+        return self.pause
+pause = Pause()
 class Shape:
     def __init__(self, letter, spawn_time):
         self.shape = pygame.Rect(0, 0, SQUARE_SIZE, SQUARE_SIZE)
@@ -58,85 +77,148 @@ class Shape:
         hitbox_bottom = HEIGHT // 8
         return self.shape.y + self.shape.height >= HEIGHT - hitbox_bottom and self.shape.y <= HEIGHT
 
-def level_1():
+def level_1(levelcsv, song, Score) -> int:
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Prototype")
     clock = pygame.time.Clock()
     background_image = pygame.image.load(os.path.join("Assets", "BG.png"))
     background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
 
+    Running = True
     shapes = []
-    SPAWN_INTERVAL = 1000
+    health = 100
     last_spawn_time = 0
-    score = 0
+    score = Score
     score_font = pygame.font.Font(None, 36)
     pygame.mixer.init()
-    background_music_file = os.path.join("Music", "Song_1.mp3")
+    background_music_file = os.path.join("Music", song)
     pygame.mixer.music.load(background_music_file)
     pygame.mixer.music.play(-1)
     flash_color = None
     flash_end_time = 0
     FLASH_DURATION = 200  # flash duration in milliseconds
+    last_time = 0
+    timedelay = 0
+    let_list = []
+    time_list = []
+    cur_index = 0
+    letter_index = 0
 
-    while True:
+    with open(levelcsv, newline='') as csvfile:
+        csvread = csv.reader(csvfile, delimiter=',')
+        line = 0
+        for row in csvread:
+            let_list.append(row[0])
+            time_list.append(row[1])
+            line = line + 1
+
+    while (Running == True):
+
+        if health <= 0:
+            return False
         correct_key_pressed = False
         pressed_key = None
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.unicode:
-                    pressed_key = event.unicode.upper()
 
-        current_time = pygame.time.get_ticks()
+        if pause.pause_state() == False:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == K_DELETE:
+                        pygame.mixer.music.pause()
+                        Running = False
+                    if event.unicode:
+                        pressed_key = event.unicode.upper()
+                    if event.key == K_BACKSPACE:
+                        level_1(levelcsv, song, Score)
+                    if event.key == K_ESCAPE:
+                        last_time = pygame.time.get_ticks()
+                        pygame.mixer.music.pause()
+                        pause.pause_game()
 
-        if pressed_key:
+            current_time = pygame.time.get_ticks()
+
+            if pressed_key:
+                for shape in shapes[:]:
+                    if shape.is_in_hitbox() and shape.letter == pressed_key:
+                        correct_key_pressed = True
+                        shapes.remove(shape)
+                        score += 10
+                        flash_color = GREEN
+                        flash_end_time = current_time + FLASH_DURATION
+                    elif shape.is_in_hitbox() and shape.letter != pressed_key:
+                        flash_color = RED
+                        health = health - 10
+                        flash_end_time = current_time + FLASH_DURATION
+
             for shape in shapes[:]:
-                if shape.is_in_hitbox() and shape.letter == pressed_key:
-                    correct_key_pressed = True
+                if shape.shape.y > HEIGHT:
                     shapes.remove(shape)
-                    score += 1
-                    flash_color = GREEN
-                    flash_end_time = current_time + FLASH_DURATION
-                elif shape.is_in_hitbox() and shape.letter != pressed_key:
-                    flash_color = RED
-                    flash_end_time = current_time + FLASH_DURATION
+                    if not correct_key_pressed and (current_time - (flash_end_time - FLASH_DURATION)) >= 350:
+                        flash_color = YELLOW
+                        health = health - 5
+                        flash_end_time = current_time + FLASH_DURATION
 
-        for shape in shapes[:]:
-            if shape.shape.y > HEIGHT:
-                shapes.remove(shape)
-                if not correct_key_pressed and (current_time - (flash_end_time - FLASH_DURATION)) >= 350:
-                    flash_color = YELLOW
-                    flash_end_time = current_time + FLASH_DURATION
 
-        if current_time - last_spawn_time >= SPAWN_INTERVAL:
-            letter = random.choice(LETTERS)
-            shapes.append(Shape(letter, current_time))
-            last_spawn_time = current_time
+            if cur_index == line:
+                spawn_interval = 5000
+            else:
+                spawn_interval = int(time_list[cur_index])
+            if current_time - last_spawn_time >= spawn_interval + timedelay:
+                timedelay = 0
+                if cur_index == line:
+                    return score
 
-        screen.blit(background_image, (0, 0))
-        bottom_rect_height = HEIGHT // 8
-        bottom_rect_width = WIDTH - 2 * VERTICAL_MARGIN_SIZE
-        bottom_rect = pygame.Surface((bottom_rect_width, bottom_rect_height), pygame.SRCALPHA)
+                else:
+                    letter = let_list[cur_index]
+                    shapes.append(Shape(letter, current_time))
+                    cur_index = cur_index + 1
+                    last_spawn_time = current_time
+                    letter_index = (letter_index + 1) % len(LETTERS)
 
-        if flash_color and current_time <= flash_end_time:
-            pygame.draw.rect(bottom_rect, flash_color, (0, 0, bottom_rect_width, bottom_rect_height))
+            screen.blit(background_image, (0, 0))
+            bottom_rect_height = HEIGHT // 8
+            bottom_rect_width = WIDTH - 2 * VERTICAL_MARGIN_SIZE
+            bottom_rect = pygame.Surface((bottom_rect_width, bottom_rect_height), pygame.SRCALPHA)
+
+            if flash_color and current_time <= flash_end_time:
+                pygame.draw.rect(bottom_rect, flash_color, (0, 0, bottom_rect_width, bottom_rect_height))
+            else:
+                pygame.draw.rect(bottom_rect, TRANSPARENT, (0, 0, bottom_rect_width, bottom_rect_height))
+
+            pygame.draw.rect(bottom_rect, WHITE, (2, 2, bottom_rect_width - 4, bottom_rect_height - 4), 2)
+            screen.blit(bottom_rect, (VERTICAL_MARGIN_SIZE, HEIGHT - bottom_rect_height))
+
+            for shape_obj in shapes:
+                shape_obj.move()
+                shape_obj.draw(screen)
+
+            score_text = score_font.render(f"Score: {score}", True, WHITE)
+            screen.blit(score_text, (10, 10))
+            health_text = score_font.render(f"Health: {health}", True, WHITE)
+            screen.blit(health_text, (10, 50))
+            pygame.display.flip()
+            clock.tick(FPS)
         else:
-            pygame.draw.rect(bottom_rect, TRANSPARENT, (0, 0, bottom_rect_width, bottom_rect_height))
-
-        pygame.draw.rect(bottom_rect, WHITE, (2, 2, bottom_rect_width - 4, bottom_rect_height - 4), 2)
-        screen.blit(bottom_rect, (VERTICAL_MARGIN_SIZE, HEIGHT - bottom_rect_height))
-
-        for shape_obj in shapes:
-            shape_obj.move()
-            shape_obj.draw(screen)
-
-        score_text = score_font.render(f"Score: {score}", True, WHITE)
-        screen.blit(score_text, (10, 10))
-        pygame.display.flip()
-        clock.tick(FPS)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == K_DELETE:
+                        pygame.mixer.music.pause()
+                        Running = False
+                    if event.key == K_BACKSPACE:
+                        pause.unpause_game()
+                        level_1(levelcsv, song, Score)
+                    if event.key == K_ESCAPE:
+                        timedelay = pygame.time.get_ticks() - last_time + timedelay
+                        last_time = 0
+                        pygame.mixer.music.unpause()
+                        pause.unpause_game()
+    return -1
 
 if __name__ == "__main__":
-    level_1()
+    print("main")
